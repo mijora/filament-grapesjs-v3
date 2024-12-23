431f937d71036f54090f7c86d69ed52570f20053
Kestutis <kestutis@mijora.lt>
2024-12-23 09:31:20 +0200

    JS manifest

--- src/FilamentGrapesJsServiceProvider.php
+++ src/FilamentGrapesJsServiceProvider.php
@@ -77,12 +77,11 @@ class FilamentGrapesJsServiceProvider extends PackageServiceProvider
 
             Js::make('grapesjs', __DIR__.'/../resources/dist/js/grapes.min.js'),
             Js::make('grapesjs-preset-webpage', __DIR__.'/../resources/dist/js/grapesjs-preset-webpage.min.js'),
-            Js::make('grapesjs-preset-webpage', __DIR__.'/../resources/dist/js/grapesjs-blocks-basic.min.js'),
-            Js::make('grapesjs-preset-webpage', __DIR__.'/../resources/dist/js/grapesjs-plugin-forms.min.js'),
-            Js::make('grapesjs-preset-webpage', __DIR__.'/../resources/dist/js/grapesjs-navbar.min.js'),
-            Js::make('grapesjs-preset-webpage', __DIR__.'/../resources/dist/js/grapesjs-lory-slider.min.js'),
-            Js::make('grapesjs-preset-webpage', __DIR__.'/../resources/dist/js/grapesjs-custom-code.min.js'),
-            Js::make('grapesjs-navbar', __DIR__.'/../resources/dist/js/grapesjs-plugin-forms.min.js'),
+            Js::make('grapesjs-blocks-basic', __DIR__.'/../resources/dist/js/grapesjs-blocks-basic.min.js'),
+            Js::make('grapesjs-plugin-form', __DIR__.'/../resources/dist/js/grapesjs-plugin-forms.min.js'),
+            Js::make('grapesjs-navbar', __DIR__.'/../resources/dist/js/grapesjs-navbar.min.js'),
+            Js::make('grapesjs-lory-slider', __DIR__.'/../resources/dist/js/grapesjs-lory-slider.min.js'),
+            Js::make('grapesjs-custom-code', __DIR__.'/../resources/dist/js/grapesjs-custom-code.min.js'),
             //Js::make('filament-grapesjs-tailwindcss', __DIR__.'/../resources/dist/js/grapesjs-tailwind.min.js'),
             Js::make('filament-grapesjs', __DIR__.'/../resources/dist/js/filament-grapesjs.js'),
         ];
