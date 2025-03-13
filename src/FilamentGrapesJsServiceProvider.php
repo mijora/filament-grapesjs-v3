bfd469f4c9bc1f572f00cb881cdef87b75ef4268
Kestutis <kestutis@mijora.lt>
2025-03-13 11:46:36 +0200

    Rename js

--- src/FilamentGrapesJsServiceProvider.php
+++ src/FilamentGrapesJsServiceProvider.php
@@ -74,7 +74,7 @@ class FilamentGrapesJsServiceProvider extends PackageServiceProvider
 
             Css::make('grapesjs', __DIR__.'/../resources/dist/css/grapes.min.css'),
             Css::make('filament-grapesjs', __DIR__.'/../resources/dist/css/filament-grapesjs.css'),
-            CSS::make('filament-grapesjs', __DIR__.'/../resources/dist/css/grapesjs-component-code-editor.min.css'),
+            CSS::make('grapesjs-component-code-editor', __DIR__.'/../resources/dist/css/grapesjs-component-code-editor.min.css'),
 
             Js::make('grapesjs', __DIR__.'/../resources/dist/js/grapes.min.js'),
             Js::make('grapesjs-preset-webpage', __DIR__.'/../resources/dist/js/grapesjs-preset-webpage.min.js'),
@@ -85,7 +85,7 @@ class FilamentGrapesJsServiceProvider extends PackageServiceProvider
             Js::make('grapesjs-custom-code', __DIR__.'/../resources/dist/js/grapesjs-custom-code.min.js'),
             Js::make('filament-grapesjs-tailwindcss', __DIR__.'/../resources/dist/js/grapesjs-tailwind.min.js'),
             Js::make('filament-grapesjs', __DIR__.'/../resources/dist/js/filament-grapesjs.js'),
-            Js::make('filament-grapesjs', __DIR__.'/../resources/dist/js/grapesjs-component-code-editor.min.js'),
+            Js::make('grapesjs-component-code-editor', __DIR__.'/../resources/dist/js/grapesjs-component-code-editor.min.js'),
         ];
     }
 
