1bdfdd5e15cbb5b71497b0ae88b4af90788d1816
Kestutis <kestutis@mijora.lt>
2025-03-18 20:03:14 +0200

    U

--- resources/js/index.js
+++ resources/js/index.js
@@ -42,7 +42,8 @@ document.addEventListener('alpine:init', () => {
 
                 var reloadHtml = () => {
                     const htmlData = (this.state ?? '').split('<---!!! STYLE !!!--->');
-                    //this.instance.setComponents(htmlData[0]);
+                    this.instance.getComponents().reset();
+                    this.instance.setComponents(htmlData[0]);
                     //this.instance.setStyle(htmlData[1]);
                     //this.instance.setScript(htmlData[2]);
                     console.log('HTML data reloaded');
