e6e890d9add411d61d94b7bfb87095d5f4f44800
Kestutis <kestutis@mijora.lt>
2025-03-19 09:22:23 +0200

    U

--- resources/js/index.js
+++ resources/js/index.js
@@ -56,7 +56,10 @@ document.addEventListener('alpine:init', () => {
                     //this.instance.setScript(htmlData[2]);
                     //console.log('HTML data reloaded');
                 };
-                window.removeEventListener("reload-grapejs");
+                window.removeEventListener("reload-grapejs", () => {
+                    console.log('Grapejs reload');
+                    reloadHtml();
+                });
                 window.addEventListener("reload-grapejs", () => {
                     console.log('Grapejs reload');
                     reloadHtml();
